85cdd8365ab1846300c87399c9e89f43773cddea
billa <billa2013@163.com>
2024-06-26 01:15:12 +0800

    Update middleware.js
    fix: 修改中间件

--- middleware.js
+++ middleware.js
@@ -2,35 +2,33 @@ import { NextResponse } from 'next/server'
 // import { getUserByJwt } from './services/user'
  
 export async function middleware(request) {
-  // let cookie = request.cookies.get('jwt')
-  // const token = cookie?.value;
-  // // console.log('~~~~~~~~~~~~~~~~~~~~middleware, token = ', token)
-  // if (process.env.AI_API_AUTH === 'N' && (request.url.indexOf('/chatProxy') > -1)) {
-  //   // console.log('不校验@@@@@@@@@@@@@@@@@@@')
-  //   return NextResponse.next()
-  // }
-  // if (token) {   
-  //   return NextResponse.next()
-  //   // const user = await getUserByJwt(jwt).catch(error => {
-  //   //   console.error('~~~~~~~~ user error = ', error)
-  //   // });
-  //   // console.log('~~~~~~~~~~~~~~~~~~~~user = ', user)
-  //   // if (user) {
-  //   //   return NextResponse.next()
-  //   // }
-  // }
-  // return new NextResponse(JSON.stringify({
-  //   code: '401',
-  //   message: '无权限访问，请先登录'
-  // }), {
-  //   status: 401
-  // })
-  return NextResponse.next()
+  let cookie = request.cookies.get('jwt')
+  const token = cookie?.value;
+  // console.log('~~~~~~~~~~~~~~~~~~~~middleware, token = ', token)
+  if (process.env.AI_API_AUTH === 'N' && (request.url.indexOf('/chatProxy') > -1)) {
+    // console.log('不校验@@@@@@@@@@@@@@@@@@@')
+    return NextResponse.next()
+  }
+  if (token) {   
+    return NextResponse.next()
+    // const user = await getUserByJwt(jwt).catch(error => {
+    //   console.error('~~~~~~~~ user error = ', error)
+    // });
+    // console.log('~~~~~~~~~~~~~~~~~~~~user = ', user)
+    // if (user) {
+    //   return NextResponse.next()
+    // }
+  }
+  return new NextResponse(JSON.stringify({
+    code: '401',
+    message: '无权限访问，请先登录'
+  }), {
+    status: 401
+  })
 }
 
 // const matcher = ['/apis/topic']
 
 export const config = {
-  // matcher: ['/apis/topic', '/chatProxy']
-  matcher: ['/apis/topic']
+  matcher: ['/apis/topic', '/chatProxy']
 }
